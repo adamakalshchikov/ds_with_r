a007cb615d9c478bd9798d8bda3869286eb1f0e3
Александр Дамакальщиков <a.damakalshchikov@gmail.com>
2018-12-11 00:03:25 +0300

    ДЗ №2
    После загрузки третировочных данных, разбил их на 3 группы:
     - от 0 до 450;
     - от 450 до 1550;
     - от 1550 до конца.
    Модель строилась по очереди на разделённых на группы тренировочных данных и исходном датасете, затем сравнивались RMSE.
    Помимо перебора тренировочных данных, после построения модели на каждой группе, функцией step выбирался оптимальный набор предикторов.

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -2,11 +2,13 @@
 home_dir <- 'C:\\Projects\\R_projects\\ds_with_r'
 setwd(home_dir)
 
+# Загружаю данные, присваиваю имена колонкам
 train_df <- read.csv('hw1/train_data.csv', sep = ';', header = T)
 test_df <- read.csv('hw1/test_data.csv', sep = ';', header = T)
 colnames(train_df) <- c('x1','x2','y')
 colnames(test_df) <- c('x1','x2','y')
 
+# factor to numeric
 test_df$x1 <- as.numeric(test_df$x1)
 test_df$x2 <- as.numeric(test_df$x2)
 test_df$y <- as.numeric(test_df$y)
@@ -15,29 +17,8 @@ train_df$x1 <- as.numeric(train_df$x1)
 train_df$x2 <- as.numeric(train_df$x2)
 train_df$y <- as.numeric(train_df$y)
 
-
-find_interval <- function(train_data, test_data, x, train_target, test_target){
-  min_rmse <- Inf
-  for (i in 1:max(x)){
-    for (j in max(x):i){
-      if (i < j){
-        cutted <- train_data[x > i & x < j, ]
-        fit <- lm(train_target ~ x, cutted)
-        prediction <- predict(fit, test_data)
-        rmse <- sum((test_target - prediction)^2)
-        if(rmse < min_rmse){
-          best_fit <- fit
-          min_rmse <- rmse
-          best_i <- i
-          best_j <- j
-        }
-      }
-    }
-  }
-}
-
-#find_interval(train_df, test_df, train_df$x1, train_df$y, test_df$y)
-
+# Разбиваю тренировочный датасет на группы 
+# после просмотра графика plot(x = train_df$x1, y = train_df$y)
 train_df_1 <- train_df[train_df$x1 < 450, ]
 train_df_2 <- train_df[train_df$x1 >= 450 & train_df$x1 < 1550,]
 train_df_3 <- train_df[train_df$x1 > 1550, ]
@@ -54,5 +35,7 @@ for (i in train_divided){
     best_data <- i
     }
 }
+
+# rmse = 15493
 
 
