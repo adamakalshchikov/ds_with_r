979105fdc1928625ea45d458be2e1660db2e9662
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2018-12-16 19:37:20 +0300

    Практика
    Построил только решаюшее дерево, для регрессии не хватило оперативки

--- desicion_tree_practice.R
+++ desicion_tree_practice.R
@@ -1,4 +1,22 @@
 clients_raw <- read.csv('Clients.txt', sep = ';', header = T)
 clients <- clients_raw
-clients[, c("GeographyKey", "CustomerKey", "SalesTerritoryKey", "HouseOwnerFlag")] <- as.factor(clients[, c("GeographyKey", "CustomerKey", "SalesTerritoryKey", "HouseOwnerFlag")])
-fit_lm <- lm(YearlyIncome ~ . , data = clients[-c("NameStyle")])
+
+# Построить модель, предсказывающую годовой доход (YearlyIncome)
+# на основе анкетных данных клиентов
+
+library(rpart)
+clients$NameStyle <- NULL
+
+#transform(clients[, c('CustomerKey','HouseOwnerFlag','SalesTerritoryKey')],int = as.factor)
+
+clients$BirthDate <- as.Date(clients$BirthDate)
+clients$DateFirstPurchase <- as.Date(clients$DateFirstPurchase)
+clients$GeographyKey <- as.factor(clients$GeographyKey)
+clients[, c("DateFirstPurchase", "BirthDate")] <- as.Date(clients[, c("DateFirstPurchase", "BirthDate")])
+fit_lm <- lm(YearlyIncome ~ . , data = clients)
+fit_tree <- rpart(YearlyIncome ~ . , data = clients)
+
+prediction_by_tree <- predict(fit_tree, clients)
+rmse_tree <- sqrt(sum((prediction_by_tree - clients$YearlyIncome)^2))
+
+#RMSE =  614082
